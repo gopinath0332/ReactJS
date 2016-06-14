3b8d24d7f70d2b4e98dccf785a05aeb000f77255
gopinath0332 <gopinath0332@outlook.com>
2016-06-14 14:41:58 +0530

    Adding jquery

--- ReactSamples/js/helloWorld.js
+++ ReactSamples/js/helloWorld.js
@@ -1,1 +1,3 @@
-ReactDOM.render(<h1>Hello there!</h1>, document.getElementById("holder"));
+// ReactDOM.render(<h1>Hello there!</h1>, document.getElementById("holder"));
+var [target] = $("#holder");
+ReactDOM.render(<h1>Hello there!</h1>, target);
