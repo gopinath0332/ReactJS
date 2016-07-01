543ee46e5a1f4ffea63253825c824860a4b87a48
gopinath0332 <gopinath0332@outlook.com>
2016-07-02 01:40:21 +0530

    Changeing route to jsx file

---
+++ ReactNpm/src/js/Route.jsx
@@ -0,0 +1,9 @@
+var React = require("react"),
+    ReactDom = require("react-dom"),
+    $ = require("jquery");
+const [targetNode] = $("#content");
+
+var nodes = [ < h1 > React Routing < /h1>, <h1>React Routing</h1 >, < h1 > React Routing < /h1>];
+
+  ReactDom.render(<div>{nodes}</div >,
+    targetNode);
--- ReactNpm/webpack.config.js
+++ ReactNpm/webpack.config.js
@@ -6,7 +6,7 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : null,
-    entry: "./js/CommentBox.jsx",
+    entry: "./js/Route.jsx",
     module: {
         loaders: [{
             test: /\.jsx?$/,
